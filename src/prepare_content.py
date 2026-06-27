5817b948550bb8c883a5b72b3c64a1741fd85d9d
Ali Abbasi <ali.abbasi797979@gmail.com>
2026-06-27 11:19:00 +0200

    Refactor search functionality to consolidate overlapping results and ensure unique context windows

--- src/prepare_content.py
+++ src/prepare_content.py
@@ -2,10 +2,14 @@
 Pull in more than 5 search results & then consulidate the overlapping results so that we have 5 unique context windows.
 """
 
-from embedding_db import TextEmbedding
+from embedding_db import get_psql_session, TextEmbedding
+from retrieve_db_content import search_embeddings, get_surrounding_sentences
+from ollama import Client
+from nltk.tokenize import sent_tokenize
+import os
 
 # Check If context window of current match overlaps with context window of existing matches.
-def is_unique_match(existing_matches, current_match, group_window_size=5):
+def _is_unique_match(existing_matches, current_match, group_window_size=5):
     for existing in existing_matches:
         if existing.file_name != current_match.file_name:
             continue
@@ -16,8 +20,37 @@ def is_unique_match(existing_matches, current_match, group_window_size=5):
     return True
 
 
+def get_filtered_matches(search_results):
+    unique_count = 0
+    matches = []
+    for result in search_results:
+        if unique_count >= 5:
+            break
+        if _is_unique_match(matches, result, group_window_size=5):
+            unique_count += 1
+            matches.append(result)
+    return matches
+
+
+def search_by_query(query, num_matches=5, group_window_size=5):
+    session = get_psql_session()
+    host = os.environ.get("OLLAMA_HOST", "http://localhost:11434")
+    client = Client(host=host)
+
+    sentences = sent_tokenize(query)
+    embeddings = client.embed(model="nomic-embed-text", input=sentences)["embeddings"]
+
+    # Pull in more than 5 search results to ensure after consulidating the overlapping results, we have 5 unique context windows.
+    search_result = search_embeddings(query_embedding=embeddings[0], session=session, limit=num_matches * 2 * group_window_size)
+    filtered_matches = get_filtered_matches(search_result)
+
+    entry_ids = [match.id for match in filtered_matches]
+    file_names = [match.file_name for match in filtered_matches]
+
+    return get_surrounding_sentences(entry_ids, file_names, session, group_window_size=group_window_size)
+
 if __name__ == "__main__":
     # test is_unique_match function
     existing_matches = [TextEmbedding(file_name="file1.txt", sentence_number=10), TextEmbedding(file_name="file1.txt", sentence_number=18)]
     current_match = TextEmbedding(file_name="file1.txt", sentence_number=14)
-    print(is_unique_match(existing_matches, current_match))
+    print(_is_unique_match(existing_matches, current_match))
